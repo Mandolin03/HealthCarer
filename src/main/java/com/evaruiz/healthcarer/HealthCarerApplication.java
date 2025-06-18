208246ddb295f5d4bf74bc71de2831f21af07c40
Mandolin03 <alcorcon03@gmail.com>
2025-06-18 10:05:06 +0200

    Initial commit

---
+++ src/main/java/com/evaruiz/healthcarer/HealthCarerApplication.java
@@ -0,0 +1,13 @@
+package com.evaruiz.healthcarer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCarerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCarerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evaruiz/healthcarer/HealthCarerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaruiz.healthcarer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCarerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
