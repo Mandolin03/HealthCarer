5c7e7f1a1315618b80bfb720c4c832e6368c9ef4
Mandolin03 <alcorcon03@gmail.com>
2025-08-08 07:24:58 +0200

    User methods
    Fixing missing methods to recognize user in docker

--- src/main/java/com/evaruiz/healthcarer/model/DTO/LoggedUser.java
+++ src/main/java/com/evaruiz/healthcarer/model/DTO/LoggedUser.java
@@ -36,4 +36,24 @@ public class LoggedUser implements UserDetails {
         return user.getEmail();
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
